c99a01bf263d9004b1880ce3db498bf59c4750a4
Thomas Hirschberger <thomas.ch.hirschberger@gmx.de>
2024-04-07 21:14:30 +0200

    added function to set ptz position of a camera; added error codes (not used at the moment); added debug flag which can be set to get more output; added root error and response to the cause of the specific error objects

--- SynoSSClient.js
+++ SynoSSClient.js
@@ -1,46 +1,113 @@
 const axios = require("axios").default;
 const https = require("https");
 
+const commondErrorCodes = {
+    100: "Unknown error",
+    101: "Invalid parameters",
+    102: "API does not exist",
+    103: "Method does not exist",
+    104: "This API version is not supported",
+    105: "Insufficient user privilege",
+    106: "Connection time out",
+    107: "Multiple login detected"
+}
+
+const authErrorCodes = {
+    100: "Unkown error",
+    101: "The account parameter is not specified",
+    400: "Invalid password",
+    401: "Guest or diabled account",
+    402: "Permission denied",
+    403: "One time password not specified",
+    404: "One time password authenticate failed",
+    405: "App portal incorrect",
+    405: "OTP code enforced",
+    407: "Max tries (if auto blocking is set to true)",
+    408: "Password Expired can not Change",
+    409: "Password Expired",
+    410: "Password must change (when first time use or after reset password by admin)",
+    411: "Account Locked (when account max try exeed)"
+}
+
+const camErrorCodes = {
+    400: "Execution failed",
+    401: "Parameter invalid",
+    402: "Camera disabled"
+}
+
+const PTZErrorCodes = {
+    400: "Execution failed",
+    401: "Parameter invalid",
+    402: "Camera disabled"
+}
+
+const PTZPresetErroCodes = {
+    400: "Operation failed",
+    401: "Parameter invalid"
+}
+
+class QueryApiVersionError extends Error {
+    constructor(message, cause) {
+        super(message);
+        this.cause = cause
+        this.name = "QueryApiVersionError";
+    }
+}
+
+class LoginError extends Error {
+    constructor(message, cause) {
+        super(message);
+        this.cause = cause
+        this.name = "LoginError";
+    }
+}
 class LogoutError extends Error {
     constructor(message, cause) {
-      super(message);
-      this.cause = cause
-      this.name = "LogoutError";
+        super(message);
+        this.cause = cause
+        this.name = "LogoutError";
     }
 }
 
 class ListCameraError extends Error {
     constructor(message, cause) {
-      super(message);
-      this.cause = cause
-      this.name = "ListCameraError";
+        super(message);
+        this.cause = cause
+        this.name = "ListCameraError";
     }
 }
 
 class GetCameraStreamInfoError extends Error {
     constructor(message, cause) {
-      super(message);
-      this.cause = cause
-      this.name = "GetCameraStreamInfoError";
+        super(message);
+        this.cause = cause
+        this.name = "GetCameraStreamInfoError";
     }
 }
 
 class ListPTZInfoError extends Error {
     constructor(message, cause) {
-      super(message);
-      this.cause = cause
-      this.name = "ListPTZInfoError";
+        super(message);
+        this.cause = cause
+        this.name = "ListPTZInfoError";
     }
 }
 
-class SetPTZPresetError extends Error {
+class GoPTZPositionError extends Error {
     constructor(message, cause) {
-      super(message);
-      this.cause = cause
-      this.name = "SetPTZPresetError";
+        super(message);
+        this.cause = cause
+        this.name = "GoPTZPositionError";
     }
 }
 
+class AutoFocusError extends Error {
+    constructor(message, cause) {
+        super(message);
+        this.cause = cause
+        this.name = "AutoFocusError";
+    }
+}
 class SynoSSClient {
     //Look-at: https://global.download.synology.com/download/Document/Software/DeveloperGuide/Package/SurveillanceStation/All/enu/Surveillance_Station_Web_API.pdf
 
@@ -59,6 +126,8 @@ class SynoSSClient {
 
     //The inital options set during construction
     #opts
+
+    #debug
 
     constructor(opts) {
         this._init(opts);
@@ -96,8 +165,12 @@ class SynoSSClient {
         }
     }
 
-    getLoginInfo(){
-        return this.#loginInfo
+    setDebugFlag(flag){
+        if(flag === true){
+            debug = true
+        } else {
+            debug = false
+        }
     }
 
     queryApiVersions(useCachedData){
@@ -106,12 +179,16 @@ class SynoSSClient {
         }
         const self = this
         if (useCachedData && (self.#apiVersions != null)){
-            console.log("Cached information about the api versions available. Using it.")
+            if (self.#debug){
+                console.log("Cached information about the api versions available. Using it.")
+            }
             return new Promise(function(myResolve, myReject) {
                     myResolve(self.#apiVersions)
                 });
         } else {
-            console.log("Either no cached api version information is available or we should not use it. Query the api version information.")
+            if (self.#debug){
+                console.log("Either no cached api version information is available or we should not use it. Query the api version information.")
+            }
             return self.#queryClient.get()
                 .then((response) => {
                     let newVersionInfo = {}
@@ -120,6 +197,8 @@ class SynoSSClient {
                     }
                     self.#apiVersions = newVersionInfo
                     return newVersionInfo
+                }).catch(error => {
+                    throw new QueryApiVersionError("Could not query the version information of the apis", {cause: { error: error, errno: error.errno, code: error.code, syscall: error.syscall, hostname: error.hostname}})
                 })
         }
     }
@@ -130,12 +209,16 @@ class SynoSSClient {
         }
         const self = this
         if (useCachedData && (self.#loginInfo != null)){
-            console.log("Login not neccessary. Using cached login information.")
+            if (self.#debug){
+                console.log("Login not neccessary. Using cached login information.")
+            }
             return new Promise(function(myResolve, myReject) {
                 myResolve(self.#loginInfo)
             });
         } else {
-            console.log("Either no cached information is available or should be used. Trying to login.")
+            if (self.#debug){
+                console.log("Either no cached information is available or should be used. Trying to login.")
+            }
 
             return self.logout(useCachedData).then( () => 
                 self.queryApiVersions(true).then(
@@ -162,8 +245,10 @@ class SynoSSClient {
                                         self.#loginInfo = {sid: response.data.data.sid, synotoken: response.data.data.synotoken}
                                         return self.#loginInfo
                                     } else {
-                                        throw new Error("Login not possible",{cause: {returnCode: response.data.error.code}})
+                                        throw new Error("Login not possible",{cause: {response: response, returnCode: response.data.error.code}})
                                     }
+                                }).catch(error => {
+                                    throw new LoginError("Could not login", {cause: { error:error, errno: error.errno, code: error.code, syscall: error.syscall, hostname: error.hostname}})
                                 })
                     }
                 )
@@ -177,7 +262,9 @@ class SynoSSClient {
         }
         const self = this
         if (self.#loginInfo != null){
-            console.log("Trying to logout.")
+            if (self.#debug){
+                console.log("Trying to logout.")
+            }
 
             return self.queryApiVersions(useCachedData).then(
                 (apiVersions) => {
@@ -203,8 +290,10 @@ class SynoSSClient {
                                         myResolve(true)
                                     });
                                 } else {
-                                    throw new LogoutError("Logout not possible. API retured with error.",{cause: {returnCode: response.data.error.code}})
+                                    throw new LogoutError("Logout not possible. API retured with error.",{cause: {response: response, returnCode: response.data.error.code}})
                                 }
+                            }).catch(error => {
+                                throw new LogoutError("Could not logout", {cause: { error:error, errno: error.errno, code: error.code, syscall: error.syscall, hostname: error.hostname}})
                             })
                 }
             )
@@ -253,11 +342,10 @@ class SynoSSClient {
 
                                     return {camIds: camIds, nameIdMapping: camNameIdMapping,idNameMapping:camIdNameMapping}
                                 } else {
-                                    throw new ListCameraError("Could not list cameras", {cause: {returnCode: response.data.error.code}})
+                                    throw new ListCameraError("Could not list cameras", {cause: {response: response, returnCode: response.data.error.code}})
                                 }
                             }).catch(error => {
-                                console.log(error)
-                                throw new ListCameraError("Could not list cameras", {cause: { errno: error.errno, code: error.code, syscall: error.syscall, hostname: error.hostname}})
+                                throw new ListCameraError("Could not list cameras", {cause: { error:error, errno: error.errno, code: error.code, syscall: error.syscall, hostname: error.hostname}})
                             }
                         )
                 }
@@ -308,11 +396,10 @@ class SynoSSClient {
 
                                     return camIdStreamMapping
                                 } else {
-                                    throw new GetCameraStreamInfoError("Could not get stream info of the cameras", {cause: {returnCode: response.data.error.code}})
+                                    throw new GetCameraStreamInfoError("Could not get stream info of the cameras", {cause: {response: response, returnCode: response.data.error.code}})
                                 }
                             }).catch(error => {
-                                console.log(error)
-                                throw new GetCameraStreamInfoError("Could not get stream info of the cameras", {cause: { errno: error.errno, code: error.code, syscall: error.syscall, hostname: error.hostname}})
+                                throw new GetCameraStreamInfoError("Could not get stream info of the cameras", {cause: { error: error, errno: error.errno, code: error.code, syscall: error.syscall, hostname: error.hostname}})
                             }
                         )
                 }
@@ -353,11 +440,59 @@ class SynoSSClient {
                                 if (response.data.success){
                                     return response.data.data.preset
                                 } else {
-                                    throw new ListPTZInfoError("Could not get PTZ info of camera with id "+camId, {cause: {returnCode: response.data.error.code}})
+                                    throw new ListPTZInfoError("Could not get PTZ info of camera with id "+camId, {cause: {response: response, returnCode: response.data.error.code}})
                                 }
                             }).catch(error => {
-                                console.log(error)
-                                throw new ListPTZInfoError("Could not get PTZ info of camera with id "+camId, {cause: { errno: error.errno, code: error.code, syscall: error.syscall, hostname: error.hostname}})
+                                throw new ListPTZInfoError("Could not get PTZ info of camera with id "+camId, {cause: { error: error, errno: error.errno, code: error.code, syscall: error.syscall, hostname: error.hostname}})
+                            }
+                        )
+                }
+            )
+        )
+    }
+
+    goPTZPosition(camId, position, useCachedData){
+        const self = this
+
+        if (typeof useCachedData === "undefined"){
+            useCachedData = true
+        }
+
+        if (typeof camId === "undefined"){
+            throw new GoPTZPositionError("Could not set PTZ preset of the camera. The camera id is missing!")
+        }
+
+        if (typeof position === "undefined"){
+            throw new GoPTZPositionError("Could not set PTZ preset of the camera. The position id is missing!")
+        }
+
+        return self.login(useCachedData).then(
+            () => self.queryApiVersions(true).then( 
+                (apiVersions) => {
+                    let api = "SYNO.SurveillanceStation.PTZ"
+                    let camPTZInfoObj = {
+                        api: api,
+                        method: "GoPreset",
+                        cameraId: camId,
+                        position: position,
+                        version: apiVersions[api],
+                        _sid: this.#loginInfo.sid,
+                        SynoToken: this.#loginInfo.synotoken
+                    }
+
+                    return this.#entryClient
+                        .get(
+                            "",{
+                                params: camPTZInfoObj,
+                            })
+                            .then((response) => {
+                                if (response.data.success){
+                                    return true
+                                } else {
+                                    throw new GoPTZPositionError("Could not set PTZ preset of camera with id "+camId, {cause: {response: reponse, returnCode: response.data.error.code}})
+                                }
+                            }).catch(error => {
+                                throw new GoPTZPositionError("Could not set PTZ preset of camera with id "+camId, {cause: { error: error, errno: error.errno, code: error.code, syscall: error.syscall, hostname: error.hostname}})
                             }
                         )
                 }
--- index.js
+++ index.js
@@ -22,18 +22,24 @@ doIt = async function(){
         this.client = new SynoSSClient(opts, "Test");
 
         let camMapping = await client.getCamIds()
-        let camIdNameMapping = camMapping.idNameMapping
+        // let camIdNameMapping = camMapping.idNameMapping
         let camIds = camMapping.camIds
         
         let getCamStreamInfo = await client.getCamStreamInfo(camIds)
         console.log(JSON.stringify(getCamStreamInfo, null, 2))
 
+        let presetInfo = {}
         for (let camIdx = 0; camIdx < camIds.length; camIdx++){
             let camId = camIds[camIdx]
             let camPTZPresetInfo = await this.client.getPTZPresetInfo(camId)
-            console.log("PTZPresetInfo of cam with id: "+camId + " and name: "+camIdNameMapping[camId]+":")
-            console.log(JSON.stringify(camPTZPresetInfo,null,2))
+            presetInfo[camId] = camPTZPresetInfo
+            // console.log("PTZPresetInfo of cam with id: "+camId + " and name: "+camIdNameMapping[camId]+":")
+            // console.log(JSON.stringify(camPTZPresetInfo,null,2))
         }
+
+        let curPTZInfo = presetInfo[2]
+        let success = await this.client.goPTZPosition(2, curPTZInfo[1].position)
+        console.log("Set PTZ success: "+success)
 
         console.log("Trying to logout (1)")
         await this.client.logout()
